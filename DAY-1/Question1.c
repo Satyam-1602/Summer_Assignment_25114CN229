2d15265d51cb0d678288f1d900ee00dce560e7c2
Satyam-1602 <satyamchauhan1602@gmail.com>
2026-06-02 08:16:20 +0530

    DAY-1 is uploaded

---
+++ DAY-1/Question1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int SumOfNaturalNumbers(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
+}
+int main(){
+    int n;
+    printf("Enter the value of N: ");
+    scanf("%d",&n);
+    int result=SumOfNaturalNumbers(n);
+    printf("The sum of first %d natural numbers is: %d\n",n,result);
+    return 0;
+}
