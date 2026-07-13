d598dda6141f7376c3eb9f49e77b243a9ca91c81
Satyam-1602 <satyamchauhan1602@gmail.com>
2026-07-13 22:21:48 +0530

    DAY-18 is uploaded

---
+++ DAY-18/Question70.c
@@ -0,0 +1,26 @@
+//Write a program to Selection sort.
+#include <stdio.h>
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        int minIndex = i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+    printf("Sorted array: \n");
+    for (int i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
